29d9e9776c057b45c758d535928178cec5101b5c
Vishal <chinchanevishal03@gmail.com>
2022-02-19 14:34:51 +0530

    completed

---
+++ index.js
@@ -0,0 +1,104 @@
+console.log('this is our 4th project in js');
+const Name = document.getElementById('Name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+
+let validEmail = false;
+let validPhone = false;
+let validUser = false;
+$('#failure').hide();
+$('#success').hide();
+
+// console.log(nav, email, phone);
+
+Name.addEventListener('blur', ()=>{
+    console.log("name is blurred");
+    // Validate name here
+    let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,15}$/;
+    let str = Name.value;
+    console.log(regex, str);
+    if(regex.test(str)){
+        console.log('Your name is valid');
+        Name.classList.remove('is-invalid');
+        validUser = true;
+    }
+    else{
+        console.log('Your name is not valid');
+        Name.classList.add('is-invalid');
+        validUser = false;
+        
+    }
+})
+
+email.addEventListener('blur', ()=>{
+    console.log("Email is blurred");
+    // Validate email here
+    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    let str = email.value;
+    console.log(regex, str);
+    if(regex.test(str)){
+        console.log('Your email is valid');
+        email.classList.remove('is-invalid');
+        validEmail = true;
+    }
+    else{
+        console.log('Your email is not valid');
+        email.classList.add('is-invalid');
+        validEmail = false;
+        
+    }
+})
+
+phone.addEventListener('blur', ()=>{
+    console.log("Phone is blurred");
+    // Validate phone here
+    let regex = /^([0-9]){10}$/;
+    let str = phone.value;
+    console.log(regex, str);
+    if(regex.test(str)){
+        console.log('Your phone is valid');
+        phone.classList.remove('is-invalid');
+        validPhone = true;
+    }
+    else{
+        console.log('Your phone is not valid');
+        phone.classList.add('is-invalid');
+        validPhone = false;
+        
+    }
+})
+
+
+
+
+let submit = document.getElementById('submit');
+submit.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    console.log('you clicked on submit');
+    
+    // Submit your form here
+    if(validEmail && validUser && validPhone){
+
+        console.log('Phone, email and user are valid. Submitting the form');
+        let success = document.getElementById('success');
+        success.classList.add('show');
+        // failure.classList.remove('show');
+        // $('#failure').alert('close');
+        $('#failure').hide();
+        $('#success').show();
+
+
+        
+    }
+    else{
+
+        console.log('One of Phone, email or user are not valid. Hence not submitting the form. Please correct the errors and try again');
+        let failure = document.getElementById('failure');
+        failure.classList.add('show');
+        // success.classList.remove('show');
+        // $('#success').alert('hide');
+        $('#success').hide();
+        $('#failure').show();
+        }
+})
